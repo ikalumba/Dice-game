ebb3e9b43684c10cc716c6cc4ec304a9bd502077
ikalumba <ikalumba221@gmail.com>
2023-11-05 13:01:16 +0300

    full functionality

--- script.js
+++ script.js
@@ -24,8 +24,10 @@ let playing = true;
 // Rolling the dice
 btnRoll.addEventListener('click', function () {
   if (playing) {
+    // getting the rolled number
     const rolled = Math.trunc(Math.random() * 6 + 1);
     console.log(rolled);
+    // Displaying image
     imgDice.src = `dice-${rolled}.png`;
     imgDice.classList.remove('hidden');
 
@@ -34,12 +36,16 @@ btnRoll.addEventListener('click', function () {
       document.querySelector(`#current--${activePlayer}`).textContent =
         currentScore;
     }
-
+    // If the rolled number is 1
     if (rolled === 1) {
       currentScore = 0;
       document.querySelector(`#current--${activePlayer}`).textContent =
         currentScore;
       activePlayer = activePlayer === 0 ? 1 : 0;
+      const nodeSec = document.querySelectorAll(`.player`);
+      for (let i = 0; i < nodeSec.length; i++) {
+        nodeSec[i].classList.toggle('player--active');
+      }
     }
   }
 });
@@ -66,27 +72,42 @@ btnHold.addEventListener('click', function () {
     }
     currentScore = 0;
 
+    // If the held score is 100+
     if (activePlayer === 0 && totalScore0 >= 100) {
       document.querySelector('.player--0').classList.add('player--winner');
 
       playing = false;
     }
 
-    if (activePlayer === 0 && totalScore0 >= 100) {
-      document.querySelector('.player--0').classList.add('player--winner');
+    if (activePlayer === 1 && totalScore1 >= 100) {
+      document.querySelector('.player--1').classList.add('player--winner');
 
       playing = false;
     }
 
     document.querySelector(`#current--${activePlayer}`).textContent = 0;
     activePlayer = activePlayer === 0 ? 1 : 0;
+    const nodeSec = document.querySelectorAll(`.player`);
+    for (let i = 0; i < nodeSec.length; i++) {
+      nodeSec[i].classList.toggle('player--active');
+    }
   }
 });
 
+// When user clicks new game - restoring the starting conditions
 btnNewGame.addEventListener('click', function () {
   let activePlayer = 0;
   let currentScore = 0;
   document.querySelector(`#score--0`).textContent = 0;
   document.querySelector(`#score--1`).textContent = 0;
   document.querySelector('.dice').classList.add('hidden');
+  totalScore0 = 0;
+  totalScore1 = 0;
+
+  document.querySelector('.player--0').classList.remove('player--winner');
+  document.querySelector('.player--1').classList.remove('player--winner');
+
+  // taking back the player active class to player 0
+  document.querySelector('.player--0').classList.add('player--active');
+  document.querySelector('.player--1').classList.remove('player--active');
 });
